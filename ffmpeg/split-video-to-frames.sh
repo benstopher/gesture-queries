7b2e84962c1067f84d707e476fb5106863a82428
Oliver Smith LCC <contact@ollyjsmith.com>
2014-08-22 11:09:58 +0100

    add script to slice video in to frames

---
+++ ffmpeg/split-video-to-frames.sh
@@ -0,0 +1,26 @@
+# takes a video and splits it into frames at a specified interval
+# example usage:
+#
+# sh ./split-video-to-frames.sh intput-file.mov 1 0 output-file-prefix
+
+INPUT_FILENAME=$1
+FRAMERATE=$2
+START_TIME=$3
+OUTPUT_PREFIX=$4
+echo ""
+echo "#####  #####    #####  #####    #####  #####    #####  #####    #####  #####"
+echo "#      #   #    #      #   #    #      #   #    #      #   #    #      #   #"
+echo "#  ##  #   #    #  ##  #   #    #  ##  #   #    #  ##  #   #    #  ##  #   #"
+echo "#   #  #   #    #   #  #   #    #   #  #   #    #   #  #   #    #   #  #   #"
+echo "#####  #####    #####  #####    #####  #####    #####  #####    #####  ##### "
+echo ""
+echo "############################################################################"
+echo ""
+echo "Splitting video ('$INPUT_FILENAME') starting at: '$START_TIME', at this rate: '$FRAMERATE', prefixing output with '$OUTPUT_PREFIX'"
+echo ""
+echo "############################################################################"
+echo ""
+
+
+###### #start time offset#input file          #same qual#framerate       #output file
+ffmpeg -i "$INPUT_FILENAME" -qscale 0 -r "$FRAMERATE" -ss "$START_TIME" "$OUTPUT_PREFIX"-%03d.png
