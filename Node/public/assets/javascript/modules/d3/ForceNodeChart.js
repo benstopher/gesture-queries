bdd2dd517d8f532bd7274013a76e5ecef2f621b6
ojs <contact@ollyjsmith.com>
2015-02-16 14:04:15 +0000

    increase force-node font size

--- Node/public/assets/javascript/modules/d3/ForceNodeChart.js
+++ Node/public/assets/javascript/modules/d3/ForceNodeChart.js
@@ -54,9 +54,6 @@ ForceNodeChart.prototype = {
 		    	that.tick()
 		    });
 
-		//this.force.nodes( this.centerNode );
-		//console.log( this.force.nodes() );
-
 	},
 	tick: function(){
 		var that = this; 
@@ -66,33 +63,31 @@ ForceNodeChart.prototype = {
 			  .attr("x2", function(d) { return d.target.x ; })
 			  .attr("y2", function(d) { return d.target.y; });
 
-			// this.node.attr("cx", function(d) { return d.x; })
-			//   .attr("cy", function(d) { return d.y; });
-			this.node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+			this.node.attr( "transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; } );
 		}
 	},
 	start: function() {
 		var that = this;
 
 		this.link = this.link.data( that.force.links(), function(d) { return d.source.id + "-" + d.target.id; });
-		this.link.enter().insert("line", ".node").attr("class", "link").attr('stroke-width', '2' ).attr("stroke", "#FFFFFF");
+		this.link.enter().insert( "line", ".node" ).attr( "class", "link" ).attr( 'stroke-width', '2' ).attr( "stroke", "#FFFFFF" );
 		this.link.exit().remove();
 
 		this.node = this.node.data( that.force.nodes(), function(d) { return d.id; });
-		this.node.enter().append("g")
-		  	.attr("class", "node")
+		this.node.enter().append( "g" )
+		  	.attr( "class", "node" )
       		.call( that.force.drag );
       	this.node.append("text")
       		.attr( "class", function(d){ return (d.center) ? "center" : "" })
-      		.attr("x", 10 )
-      		.attr("fill", "#ffffff" )
-      		.attr("font-family", "'Executive'" )
-      		.attr("font-size", "16")
+      		.attr( "x", 10 )
+      		.attr( "fill", "#ffffff" )
+      		.attr( "font-family", "'Executive'" )
+      		.attr( "font-size", "32" )
       		.text(function(d) { return d.word.toUpperCase() });
       	this.node.append( "circle" )
-      		.attr("cx", -3)
-      		.attr("cy", -3)
-      		.attr("r", 6)
+      		.attr( "cx", -3)
+      		.attr( "cy", -3)
+      		.attr( "r", 6)
       		.attr( "fill", "#FFFFFF" )
 		this.node.exit().remove();
 
@@ -126,52 +121,6 @@ ForceNodeChart.prototype = {
 
 		that.force.nodes( this.nodes );
 		that.force.links( this.links );
-
-		//var pCenter = this.force.nodes()[0];
-		//var pNodes = this.force.nodes();
-		// for( var i = 0; i < data.links.length; i++ ){
-		// 	data.links[i].source = data.links[i].source + pNodes.length;
-		// 	data.links[i].target = data.links[i].target + pNodes.length;
-		// }
-		// for( var i = 0; i < data.nodes.length; i++ ){
-		// 	if( data.nodes[i].center === true ){
-		// 		data.links.push({ source: pNodes.length, target: 0 })
-		// 	}
-		// 	pNodes.push( data.nodes[i] );
-		// }
-		
-		// data.links.push({
-		// 	source: 
-		// })
-		// this.force
-		//   .nodes( pNodes )
-		//   .links( data.links )
-
-
-		// this.link.remove();
-		//this.node.remove();
-		
-
-
-		// this.link = this.link.data( data.links )
-		// 	.enter().append("line")
-		//   	.attr("class", "link");
-
-		// this.node = this.node.data( data.nodes) 
-		// 	.enter().append("g")
-		//   	.attr("class", "node")
-  //     		.call( that.force.drag );
-
-		// this.node.append("text")
-  //     		.attr( "class", function(d){ return (d.center) ? "center" : "" })
-  //     		.attr("x", 10 )
-  //     		.text(function(d) { return d.word });
-
-  //     	this.node.append( "circle" )
-  //     		.attr("x", -3)
-  //     		.attr("y", -3)
-  //     		.attr("r", 6);
-      	  
 
 		this.start();
 
